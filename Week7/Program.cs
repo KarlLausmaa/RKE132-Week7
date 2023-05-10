3a7b0e8a62553a095f338832bdc01fd65b2e36cd
Karl Lausmaa <karl.lausmaa@gmail.com>
2023-05-10 17:08:46 +0300

    week 7 task

---
+++ Week7/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Coming or leaving? (in/out)");
+
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+//PrintHello();
+//PrintGoodBye();
+
+static void PrintHello()
+{
+    Console.WriteLine("Hi");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("Bye");
+}
